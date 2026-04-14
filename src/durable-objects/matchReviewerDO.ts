b9edbd8f602d547a495be1b4f631a07af641b020
peterkruhoffer <peter.kruhoffer@gmail.com>
2026-04-14 19:51:50 +0200

    Add sqlite DO storage

--- src/durable-objects/matchReviewerDO.ts
+++ src/durable-objects/matchReviewerDO.ts
@@ -2,8 +2,17 @@ import { DurableObject } from "cloudflare:workers";
 import { ParsedLogResponse, parseMatchLog } from "@/parser/parser";
 
 export class MatchReviewerDO extends DurableObject<CloudflareBindings> {
+  sql: SqlStorage;
   constructor(ctx: DurableObjectState, env: CloudflareBindings) {
     super(ctx, env);
+
+    this.sql = ctx.storage.sql;
+    this.sql.exec(`CREATE TABLE IF NOT EXISTS review(
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          imported_at TEXT NOT NULL,
+          review_json TEXT NOT NULL
+    );
+`);
   }
 
   async getMatchLog(url: string): Promise<ParsedLogResponse | null> {
@@ -20,10 +29,27 @@ export class MatchReviewerDO extends DurableObject<CloudflareBindings> {
     }
 
     const log = await response.text();
+    const importedAt = new Date().toISOString();
     const report = parseMatchLog(log);
     if (report === null) {
       return null;
     }
+
+    this.ctx.storage.sql.exec(
+      `
+        INSERT OR REPLACE INTO review (imported_at, review_json)
+        VALUES (?, ?)
+      `,
+      importedAt,
+      JSON.stringify(report),
+    );
+
     return report;
+  }
+
+  async clearDo(): Promise<void> {
+    // This will delete all the storage associated with this Durable Object instance
+    // This will also delete the Durable Object instance itself
+    await this.ctx.storage.deleteAll();
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,8 @@ app.get("/", (c) => {
   });
 });
 
+const NAME_OF_DO = "matchreviewer";
+
 app.get("/matches", async (c) => {
   const url = c.req.query("url");
   if (!url) {
@@ -25,8 +27,8 @@ app.get("/matches", async (c) => {
   }
   // Will always hit the same instance of Durable Object
   // Can be made to be diffrent per match if needed/wanted by making the name diffrent every request
-  // Current usecase i 1 match
-  const id = c.env.MATCH_REVIEWER.idFromName("matchreviewer");
+  // Current usecase is 1 match
+  const id = c.env.MATCH_REVIEWER.idFromName(NAME_OF_DO);
   const stub = c.env.MATCH_REVIEWER.get(id);
   const matchLog = await stub.getMatchLog(url);
 
@@ -35,6 +37,14 @@ app.get("/matches", async (c) => {
   }
 
   return c.json(matchLog);
+});
+
+app.delete("/admin/clear", async (c) => {
+  const id = c.env.MATCH_REVIEWER.idFromName(NAME_OF_DO);
+  const stub = c.env.MATCH_REVIEWER.get(id);
+  await stub.clearDo();
+
+  return c.json({ message: "Data has been cleared" });
 });
 
 export { MatchReviewerDO };
